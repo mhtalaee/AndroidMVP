b3e447d8103182d72a1043994557bbbd6e9d08dc
Talaee <mhtalaee@gmail.com>
2019-05-18 16:31:55 +0430

    add Firebase push notification

---
+++ app/src/main/java/ir/goldenmind/androidmvp/base/MyFirebaseMessagingService.kt
@@ -0,0 +1,10 @@
+package ir.goldenmind.androidmvp.base
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onNewToken(p0: String?) {
+        super.onNewToken(p0)
+    }
+}
--- app/src/main/java/ir/goldenmind/androidmvp/features/addcontact/AddContactActivity.kt
+++ app/src/main/java/ir/goldenmind/androidmvp/features/addcontact/AddContactActivity.kt
@@ -2,9 +2,13 @@ package ir.goldenmind.androidmvp.features.addcontact
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
 import ir.goldenmind.androidmvp.R
 import ir.goldenmind.androidmvp.base.BaseActivity
+import ir.goldenmind.androidmvp.base.MyFirebaseMessagingService
 import ir.goldenmind.androidmvp.pojo.Contact
 import kotlinx.android.synthetic.main.activity_add_contact.*
 
@@ -16,6 +20,23 @@ class AddContactActivity : BaseActivity(), Contract.View {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_contact)
         init()
+
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w("FireBase", "getInstanceId failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token
+                Log.d("Token", token)
+
+            })
+
+
+
     }
 
     override fun onResume() {
